eaf4e160e1f2def8df7e668b34e45ccc0e1133e6
tranguixdev <trang.uixdev@gmail.com>
2024-04-26 11:51:32 +0900

    Set colors in tailwindconfig.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      color:{
+        primary:'#0b1600'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
